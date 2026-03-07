610390570cc9833450a02747e5adce769cff0f33
Dio Soetarman <aldiocs2@uic.edu>
2026-03-06 18:20:58 -0600

    Restructure project files and folders, added test folder with a parser_test file

---
+++ Project1/src/parser.py
@@ -0,0 +1,76 @@
+"""The main parser for our project."""
+
+import pandas as pd
+
+
+
+def read_file_subset(filepath, subset=[]):
+    """reads the given CSV and returns a dataframe
+
+        Input: 
+            - Filepath : path to the file
+            - subset : LIST of column names that you want to keep (default is all)
+        Output:
+            - Dataframe with your csv read, data is NOT cleaned
+
+            TODO : change all them prints to logs when we get that set up
+    """
+
+    # read the file
+    try:
+        df = pd.read_csv(filepath)
+    except pd.errors.EmptyDataError:
+        print("File is empty.")
+        return None
+    except pd.errors.ParserError as e:
+        print("Error parsing the CSV file.")
+        raise RuntimeError("Something went wrong with the parsing, try again e:",e)
+    except FileNotFoundError as e:
+        print("File not found.")
+        raise FileNotFoundError("Filepath wrong e:",e)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        exit(-1) # quit now, we need to fix this
+
+    # extract subset of data we want to work with if any
+    if not subset:
+        return df
+    else:
+        return df[subset]
+    
+
+def clean_data(df):
+    """Cleans the given Dataframe
+    
+    -Converts numerical entries to Int/Float
+    - TODO decide if there are too many NaN entries and if we need to drop a col, or do we fill
+    - removes duplicate rows, strip whitespace and standardize text
+    
+    """
+    # Our subset in THIS dataset
+    # age (int) ,monthly_income (float ),daily_internet_hours(float),smartphone_usage_years(float),social_media_hours(float),
+    # online_payment_trust_score(float),tech_savvy_score(float),monthly_online_orders(int),monthly_store_visits(int),
+    # avg_online_spend(float),shopping_preference(string)
+    #
+
+    # TODO decide whether to drop anything here
+
+
+
+    # type conversions
+    df["age"] = df["age"].astype(int)
+    df["monthly_income"] = df["monthly_income"].astype(float)
+    df["daily_internet_hours"] = df["daily_internet_hours"].astype(float)
+    df["smartphone_usage_years"] = df["smartphone_usage_years"].astype(float)
+    df["social_media_hours"] = df["social_media_hours"].astype(float)
+    df["online_payment_trust_score"] = df["online_payment_trust_score"].astype(float)
+    df["tech_savvy_score"] = df["tech_savvy_score"].astype(float)
+    df["monthly_online_orders"] = df["monthly_online_orders"].astype(int)
+    df["monthly_store_visits"] = df["monthly_store_visits"].astype(int)
+    df["avg_online_spend"] = df["avg_online_spend"].astype(float)
+    df["shopping_preference"] = df["shopping_preference"].astype(str)
+    
+    # string standardizing
+    df["shopping_preference"] = df["shopping_preference"].applymap(lambda x : x.strip().lower())
+
+    return df
---
+++ Project1/tests/parser_test.py
@@ -0,0 +1,1 @@
+"""File to test parser.py functionality"""
