3d3e59a09ad7387722b0c235065d817642f5a1ca
Jaisal <jkmehta@iu.edu>
2026-03-11 12:59:28 -0500

    added comments to CreateTables.sql and parser_test.py, also added more tests in parser_py against the test_subset.csv file

--- Project1/src/SQL/CreateTables.sql
+++ Project1/src/SQL/CreateTables.sql
@@ -1,11 +1,18 @@
+-- creating database
 CREATE DATABASE IF NOT EXISTS Shopping_Data;
 
+-- creating Customers table
+-- PK: customer_id
 DROP IF EXISTS Customers
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     age INT NOT NULL,
     monthly_income FLOAT NOT NULL
 );
+
+-- creating Technology_Usage table
+-- PK: technology_usage_id
+-- FK: customer_id from Customers table
 DROP IF EXISTS Technology_Usage
 CREATE TABLE Technology_Usage (
     technology_usage_id SERIAL PRIMARY KEY,
@@ -14,6 +21,10 @@ CREATE TABLE Technology_Usage (
     smartphone_usage_years FLOAT NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- creating Social_Behavior table
+-- PK: social_behavior_id
+-- FK: customer_id from Customers table
 DROP IF EXISTS Social_Behavior
 CREATE TABLE Social_Behavior (
     social_behavior_id SERIAL PRIMARY KEY,
@@ -24,7 +35,19 @@ CREATE TABLE Social_Behavior (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-DROP IF EXISTS DROP IF EXISTS
+-- creating Shopping_Preference table
+-- PK: shopping_preference_id
+DROP IF EXISTS Shopping_Preference
+CREATE TABLE Shopping_Preference (
+    shopping_preference_id SERIAL PRIMARY KEY,
+    preference_name VARCHAR(50) NOT NULL
+);
+
+-- creating Shopping_Behavior table
+-- PK: shopping_behavior_id
+-- FK: customer_id from Customers table
+-- FK: shopping_preference_id from Shopping_Preference table
+DROP IF EXISTS Shopping_Behavior
 CREATE TABLE Shopping_Behavior (
     shopping_behavior_id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -36,13 +59,8 @@ CREATE TABLE Shopping_Behavior (
     FOREIGN KEY (shopping_preference_id) REFERENCES Shopping_Preference(shopping_preference_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-DROP IF EXISTS Shopping_Preference
-CREATE TABLE Shopping_Preference (
-    shopping_preference_id SERIAL PRIMARY KEY,
-    preference_name VARCHAR(50) NOT NULL
-);
-
+-- creating Invalid_Entries table
+-- PK: invalid_entry_id
 DROP IF NOT EXISTS Invalid_Entries
 CREATE TABLE Invalid_Entries ( -- entries containing null values or were duplicates
     invalid_entry_id SERIAL PRIMARY KEY,
